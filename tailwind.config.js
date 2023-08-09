6c0df68123d8253ec73f01ebcb71b7296ebcd425
Suphakit P <me@suphakit.net>
2023-08-10 02:13:46 +0700

    chore<tailwind>: update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,11 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		colors: {
-			'pastel-black': '#2e2f2f',
-			whitesmoke: '#f5f5f5',
+			'pastel-black': '#0A0A0A',
+			'jet': '#2e2f2f',
+			'whitesmoke': '#f5f5f5',
 			'living-coral': '#fe6e61',
-			overlay: 'rgba(0,0,0,0.4)'
+			'overlay': 'rgba(0,0,0,0.4)'
 		},
 		borderRadius: {
 			DEFAULT: '1rem'
